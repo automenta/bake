f71994628493ff79fe542e6201e780ac6e95e20e
SeH <1s1e1h1@gmail.com>
2014-12-06 03:32:23 -0500

    simplifications and build.xml stub

--- bake.js
+++ bake.js
@@ -1,8 +1,42 @@
 
+var bake_home = "."; //arguments[0];
+
+
+var prefix = "*** bake: ";
+
+///////
+ant_imports = new JavaImporter(
+        org.apache.tools.ant,
+        org.apache.tools.ant.types,
+        org.apache.tools.ant.taskdefs);
+
+ant_helper = {
+    call_task: function(taskClass, properties) {
+        var antTask = new taskClass;
+        for (var property in properties) {
+            var methodName = "set" + property.substr(0, 1).toUpperCase() + property.substr(1);
+            var value = properties[property];
+            antTask[methodName](value);
+        }
+        antTask.setProject(new ant_imports.Project());
+        antTask.execute();
+    },
+
+    create_path: function(pathString) {
+        var path = new ant_imports.Path(new ant_imports.Project());
+        path.setLocation(new java.io.File(pathString));
+        return path;
+    }
+};
+
+
+
+
+//////
 bake_helper = {
 
     print_targets: function() {
-        println("** bake: Found the following targets...");
+        println(prefix + "Found the following targets...");
         for (i in project) {
             var line = i;
             if (project[i].description) {
@@ -14,9 +48,14 @@ bake_helper = {
 
     run_task: function(task_name) {
 		var task = project[task_name];
-
+        
+        if (!task) {
+            println("Task '" + task_name + "' does not exist");
+            exit(1);
+        }
+        
 		if (task.depends) {
-    		println("** bake: Checking dependencies for '" + task_name + "'");
+    		println(prefix + "Checking dependencies for '" + task_name + "'");
 		    for (i in task.depends) {
 		        var dependentTarget = task.depends[i];
 		        bake_helper.run_task(dependentTarget);
@@ -24,11 +63,11 @@ bake_helper = {
 		}
 
         if (bake_helper.check_if_needed(task)) {
-    		println("** bake: Running task '" + task_name + "'");
+    		println(prefix + "Running task '" + task_name + "'");
             task.build();
         }
         else {
-    		println("** bake: Turns out '" + task_name + "' doesn't need to run");
+    		println(prefix + "Turns out '" + task_name + "' doesn't need to run");
         }
     },
 
@@ -92,3 +131,32 @@ bake_helper = {
     }
 
 };
+
+
+
+// parse build file
+try {
+	load("build.js");
+}
+catch (e if e.javaException instanceof javax.script.ScriptException) {
+    println(prefix + "There was an error parsing your build file: " + e);
+    exit(1);
+}
+catch (e) {
+	println(prefix + "Couldn't load 'build.js' in the current directory");
+	// TODO: "...do you want me to create one?"
+	exit(1);
+}
+
+    
+// print targets if there are no args
+if (arguments.length < 1) {
+	bake_helper.print_targets();
+	exit(0);
+}
+
+// run target
+bake_helper.run_task(arguments[0]);
+
+println(prefix + "Finished.");
+
